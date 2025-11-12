156dc0f255979afd03695bf1233c5499f8ee356f
c00309423 <c00309423@setu.ie>
2025-11-12 14:12:06 +0000

    renamed variables

--- 16_SFML_Games/arkanoid.cpp
+++ 16_SFML_Games/arkanoid.cpp
@@ -9,64 +9,64 @@ int arkanoid()
     RenderWindow app(VideoMode(520, 450), "Arkanoid!");
     app.setFramerateLimit(60);
 
-    Texture t1,t2,t3,t4;
-    t1.loadFromFile("images/arkanoid/block01.png");
-    t2.loadFromFile("images/arkanoid/background.jpg");
-    t3.loadFromFile("images/arkanoid/ball.png");
-    t4.loadFromFile("images/arkanoid/paddle.png");
+    Texture blockTexture,backGroundTexture,ballTexture,paddleTexture;
+    blockTexture.loadFromFile("images/arkanoid/block01.png");
+    backGroundTexture.loadFromFile("images/arkanoid/background.jpg");
+    ballTexture.loadFromFile("images/arkanoid/ball.png");
+    paddleTexture.loadFromFile("images/arkanoid/paddle.png");
 
-    Sprite sBackground(t2), sBall(t3), sPaddle(t4);
+    Sprite sBackground(backGroundTexture), sBall(ballTexture), sPaddle(paddleTexture);
     sPaddle.setPosition(300,440);
 
     Sprite block[1000];
 
-    int n=0;
-    for (int i=1;i<=10;i++)
+    int blockIndex=0;
+    for (int index=1;index<=10;index++)
     for (int j=1;j<=10;j++)
       {
-         block[n].setTexture(t1);
-         block[n].setPosition(i*43,j*20);
-         n++;
+         block[blockIndex].setTexture(blockTexture);
+         block[blockIndex].setPosition(index*43,j*20);
+         blockIndex++;
       }
 
-    float dx=6, dy=5;
-    float x=300, y=300;
+    float horizontalMovement=6, verticalMovement=5;
+    float xPos=300, yPos=300;
 
     while (app.isOpen())
     {
-       Event e;
-       while (app.pollEvent(e))
+       Event event;
+       while (app.pollEvent(event))
        {
-         if (e.type == Event::Closed)
+         if (event.type == Event::Closed)
              app.close();
        }
 
-    x+=dx;
-    for (int i=0;i<n;i++)
-        if ( FloatRect(x+3,y+3,6,6).intersects(block[i].getGlobalBounds()) ) 
-             {block[i].setPosition(-100,0); dx=-dx;}
+    xPos+=horizontalMovement;
+    for (int i=0;i<blockIndex;i++)
+        if ( FloatRect(xPos+3,yPos+3,6,6).intersects(block[i].getGlobalBounds()) ) 
+             {block[i].setPosition(-100,0); horizontalMovement=-horizontalMovement;}
 
-    y+=dy;
-    for (int i=0;i<n;i++)
-        if ( FloatRect(x+3,y+3,6,6).intersects(block[i].getGlobalBounds()) ) 
-             {block[i].setPosition(-100,0); dy=-dy;}
+    yPos+=verticalMovement;
+    for (int i=0;i<blockIndex;i++)
+        if ( FloatRect(xPos+3,yPos+3,6,6).intersects(block[i].getGlobalBounds()) ) 
+             {block[i].setPosition(-100,0); verticalMovement=-verticalMovement;}
 
-    if (x<0 || x>520)  dx=-dx;
-    if (y<0 || y>450)  dy=-dy;
+    if (xPos<0 || xPos>520)  horizontalMovement=-horizontalMovement;
+    if (yPos<0 || yPos>450)  verticalMovement=-verticalMovement;
 
     if (Keyboard::isKeyPressed(Keyboard::Right)) sPaddle.move(6,0);
     if (Keyboard::isKeyPressed(Keyboard::Left)) sPaddle.move(-6,0);
 
-    if ( FloatRect(x,y,12,12).intersects(sPaddle.getGlobalBounds()) ) dy=-(rand()%5+2);
+    if ( FloatRect(xPos,yPos,12,12).intersects(sPaddle.getGlobalBounds()) ) verticalMovement=-(rand()%5+2);
 
-    sBall.setPosition(x,y);
+    sBall.setPosition(xPos,yPos);
 
     app.clear();
     app.draw(sBackground);
     app.draw(sBall);
     app.draw(sPaddle);
 
-    for (int i=0;i<n;i++)
+    for (int i=0;i<blockIndex;i++)
      app.draw(block[i]);
 
     app.display();
