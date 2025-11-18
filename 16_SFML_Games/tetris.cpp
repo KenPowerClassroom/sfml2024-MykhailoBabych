2ecd2d729fa0158bfbf34147eee795ee47271acb
MykhailoBabych <C00309423@setu.ie>
2025-11-18 18:46:15 +0000

    created structs

--- 16_SFML_Games/tetris.cpp
+++ 16_SFML_Games/tetris.cpp
@@ -4,16 +4,22 @@ using namespace sf;
 
 const int MAX_ROWS = 20;
 const int MAX_COLUMNS = 10;
-int deltaX = 0;
-bool rotate = 0;
-int colorNum = 1;
-float timer = 0;
-float delay = 0.3;
 
-int field[MAX_ROWS][MAX_COLUMNS] = {0};
+struct Point { int x, y; };
 
-struct Point
-{int x,y;} coordinates[4], backup_coordinates[4];
+struct GameState
+{
+    int deltaX = 0;
+    bool rotate = false;
+    int colorNum = 1;
+    float timer = 0;
+    float delay = 0.3;
+
+    int field[MAX_ROWS][MAX_COLUMNS] = { 0 };
+
+    Point coordinates[4];
+    Point backup_coordinates[4];
+};
 
 int figures[7][4] =
 {
@@ -26,136 +32,133 @@ int figures[7][4] =
     2,3,4,5, // O
 };
 
-bool check()
+bool check(GameState& state)
 {
-    for (int i = 0;i < 4;i++)
+    for (int i = 0; i < 4; i++)
     {
-        if (coordinates[i].x < 0 || coordinates[i].x >= MAX_COLUMNS || coordinates[i].y >= MAX_ROWS)
-        {
+        if (state.coordinates[i].x < 0 || state.coordinates[i].x >= MAX_COLUMNS ||
+            state.coordinates[i].y >= MAX_ROWS)
             return false;
-        }
-        else if (field[coordinates[i].y][coordinates[i].x])
-        {
+
+        if (state.field[state.coordinates[i].y][state.coordinates[i].x])
             return false;
-        }
     }
-   return true;
+    return true;
 }
 
-void move()
+void move(GameState& state)
 {
-    for (int i = 0;i < 4;i++)
+    for (int i = 0; i < 4; i++)
     {
-        backup_coordinates[i] = coordinates[i];
-        coordinates[i].x += deltaX;
+        state.backup_coordinates[i] = state.coordinates[i];
+        state.coordinates[i].x += state.deltaX;
     }
-    if (!check())
+    if (!check(state))
     {
-        for (int i = 0;i < 4;i++)
-        {
-            coordinates[i] = backup_coordinates[i];
-        }
+        for (int i = 0; i < 4; i++)
+            state.coordinates[i] = state.backup_coordinates[i];
     }
 }
 
-void rotation()
+void rotation(GameState& state)
 {
-    if (rotate)
+    if (state.rotate)
     {
-        Point rotationCenter = coordinates[1]; //center of rotation
-        for (int i = 0;i < 4;i++)
+        Point center = state.coordinates[1];
+        for (int i = 0; i < 4; i++)
         {
-            int x = coordinates[i].y - rotationCenter.y;
-            int y = coordinates[i].x - rotationCenter.x;
-            coordinates[i].x = rotationCenter.x - x;
-            coordinates[i].y = rotationCenter.y + y;
+            int x = state.coordinates[i].y - center.y;
+            int y = state.coordinates[i].x - center.x;
+            state.coordinates[i].x = center.x - x;
+            state.coordinates[i].y = center.y + y;
         }
-        if (!check())
+        if (!check(state))
         {
-            for (int i = 0;i < 4;i++)
-            {
-                coordinates[i] = backup_coordinates[i];
-            }
+            for (int i = 0; i < 4; i++)
+                state.coordinates[i] = state.backup_coordinates[i];
         }
     }
 }
 
-void ticking()
+void ticking(GameState& state)
 {
-    if (timer > delay)
+    if (state.timer > state.delay)
     {
-        for (int i = 0;i < 4;i++)
+        for (int i = 0; i < 4; i++)
         {
-            backup_coordinates[i] = coordinates[i];
-            coordinates[i].y += 1;
+            state.backup_coordinates[i] = state.coordinates[i];
+            state.coordinates[i].y += 1;
         }
 
-        if (!check())
+        if (!check(state))
         {
-            for (int i = 0;i < 4;i++)
-            {
-                field[backup_coordinates[i].y][backup_coordinates[i].x] = colorNum;
-            }
+            for (int i = 0; i < 4; i++)
+                state.field[state.backup_coordinates[i].y][state.backup_coordinates[i].x] = state.colorNum;
 
-            colorNum = 1 + rand() % 7;
-            int nextTetrominoShape = rand() % 7;
-            for (int i = 0;i < 4;i++)
+            state.colorNum = 1 + rand() % 7;
+            int nextShape = rand() % 7;
+
+            for (int i = 0; i < 4; i++)
             {
-                coordinates[i].x = figures[nextTetrominoShape][i] % 2;
-                coordinates[i].y = figures[nextTetrominoShape][i] / 2;
+                state.coordinates[i].x = figures[nextShape][i] % 2;
+                state.coordinates[i].y = figures[nextShape][i] / 2;
             }
         }
 
-        timer = 0;
+        state.timer = 0;
     }
 }
 
-void lineCheck()
+void lineCheck(GameState& state)
 {
-	int destRow = MAX_ROWS - 1;
-	for (int i = MAX_ROWS - 1;i > 0;i--)
-	{
-		int count = 0;
-		for (int j = 0;j < MAX_COLUMNS;j++)
-		{
-			if (field[i][j])
-			{
-				count++;
-			}
-			field[destRow][j] = field[i][j];
-		}
-		if (count < MAX_COLUMNS)
-		{
-			destRow--;
-		}
-	}
+    int destRow = MAX_ROWS - 1;
+    for (int i = MAX_ROWS - 1; i >= 0; i--)
+    {
+        int count = 0;
+        for (int j = 0; j < MAX_COLUMNS; j++)
+        {
+            if (state.field[i][j]) count++;
+            state.field[destRow][j] = state.field[i][j];
+        }
+        if (count < MAX_COLUMNS)
+            destRow--;
+    }
 }
 
-void reset() 
+void reset(GameState& state)
 {
-    deltaX = 0;
-    rotate = 0;
-    delay = 0.3;
+    state.deltaX = 0;
+    state.rotate = false;
+    state.delay = 0.3;
 }
-
 
 int tetris()
 {
-    srand(time(0));     
+    srand(time(0));
+
+    GameState state;
+
+    // ------------------------------------
+    // INITIALIZE FIRST TETROMINO
+    // ------------------------------------
+    int shape = rand() % 7;
+    for (int i = 0; i < 4; i++)
+    {
+        state.coordinates[i].x = figures[shape][i] % 2;
+        state.coordinates[i].y = figures[shape][i] / 2;
+    }
+    // ------------------------------------
 
     RenderWindow window(VideoMode(320, 480), "The Game!");
 
-    Texture m_tilesTexture;
-    Texture m_backgroundTexture;
-    Texture m_frameTexture;
-    m_tilesTexture.loadFromFile("images/tetris/tiles.png");
-    m_backgroundTexture.loadFromFile("images/tetris/background.png");
-    m_frameTexture.loadFromFile("images/tetris/frame.png");
+    Texture tilesTexture, backgroundTexture, frameTexture;
+    tilesTexture.loadFromFile("images/tetris/tiles.png");
+    backgroundTexture.loadFromFile("images/tetris/background.png");
+    frameTexture.loadFromFile("images/tetris/frame.png");
 
-
-    Sprite m_tilesSprite(m_tilesTexture);
-    Sprite m_backgroundSprite(m_backgroundTexture);
-    Sprite m_frameSprite(m_frameTexture);
+    Sprite tilesSprite(tilesTexture);
+    Sprite backgroundSprite(backgroundTexture);
+    Sprite frameSprite(frameTexture);
 
     Clock clock;
 
@@ -163,83 +166,59 @@ int tetris()
     {
         float time = clock.getElapsedTime().asSeconds();
         clock.restart();
-        timer += time;
+        state.timer += time;
 
-        Event m_event;
-        while (window.pollEvent(m_event))
+        Event e;
+        while (window.pollEvent(e))
         {
-            if (m_event.type == Event::Closed)
-            {
+            if (e.type == Event::Closed)
                 window.close();
-            }
 
-            if (m_event.type == Event::KeyPressed)
+            if (e.type == Event::KeyPressed)
             {
-                if (m_event.key.code == Keyboard::Up)
-                {
-                    rotate = true;
-                }
-                else if (m_event.key.code == Keyboard::Left)
-                {
-                    deltaX = -1;
-                }
-                else if (m_event.key.code == Keyboard::Right)
-                {
-                    deltaX = 1;
-                }
+                if (e.key.code == Keyboard::Up) state.rotate = true;
+                else if (e.key.code == Keyboard::Left) state.deltaX = -1;
+                else if (e.key.code == Keyboard::Right) state.deltaX = 1;
             }
         }
 
         if (Keyboard::isKeyPressed(Keyboard::Down))
+            state.delay = 0.05;
+
+        move(state);
+        rotation(state);
+        ticking(state);
+        lineCheck(state);
+        reset(state);
+
+        window.clear(Color::White);
+        window.draw(backgroundSprite);
+
+        // draw field
+        for (int i = 0; i < MAX_ROWS; i++)
         {
-            delay = 0.05;
-        }
-
-    //// <- Move -> ///
-        move();
-
-    //////Rotate//////
-        rotation();
-
-    ///////Tick//////
-        ticking();
-
-    ///////check lines//////////
-		lineCheck();
-
-        reset();
-
-    /////////draw//////////
-    window.clear(Color::White);    
-    window.draw(m_backgroundSprite);
-          
-    for (int i = 0;i < MAX_ROWS;i++)
-    {
-        for (int column_index = 0;column_index < MAX_COLUMNS;column_index++)
-        {
-            if (field[i][column_index] == 0)
+            for (int j = 0; j < MAX_COLUMNS; j++)
             {
-                continue;
+                if (!state.field[i][j]) continue;
+                tilesSprite.setTextureRect(IntRect(state.field[i][j] * 18, 0, 18, 18));
+                tilesSprite.setPosition(j * 18, i * 18);
+                tilesSprite.move(28, 31);
+                window.draw(tilesSprite);
             }
-            m_tilesSprite.setTextureRect(IntRect(field[i][column_index] * 18, 0, 18, 18));
-            m_tilesSprite.setPosition(column_index * 18, i * 18);
-            m_tilesSprite.move(28, 31); //offset
-            window.draw(m_tilesSprite);
         }
-    }
 
-    for (int i=0;i<4;i++)
-    {
-       m_tilesSprite.setTextureRect(IntRect(colorNum*18,0,18,18));
-       m_tilesSprite.setPosition(coordinates[i].x*18,coordinates[i].y*18);
-       m_tilesSprite.move(28,31); //offset
-       window.draw(m_tilesSprite);
-    }
+        // draw current tetromino
+        for (int i = 0; i < 4; i++)
+        {
+            tilesSprite.setTextureRect(IntRect(state.colorNum * 18, 0, 18, 18));
+            tilesSprite.setPosition(state.coordinates[i].x * 18, state.coordinates[i].y * 18);
+            tilesSprite.move(28, 31);
+            window.draw(tilesSprite);
+        }
 
-    window.draw(m_frameSprite);
-    window.display();
+        window.draw(frameSprite);
+        window.display();
     }
 
     return 0;
 }
-
